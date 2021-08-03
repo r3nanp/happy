781f319e630afaa82f09ad9bc9ffa00358d1380a
Renan Pereira <renanmol87@gmail.com>
2021-08-03 14:01:32 -0300

    style: create theme and global styles

---
+++ src/styles/global.ts
@@ -0,0 +1,47 @@
+import {
+  createGlobalStyle,
+  css,
+  DefaultTheme,
+  GlobalStyleComponent
+} from 'styled-components'
+
+type GlobalStylesProps = {
+  removeBg?: boolean
+}
+
+export const GlobalStyle: GlobalStyleComponent<
+  GlobalStylesProps,
+  DefaultTheme
+> = createGlobalStyle`
+  ${({ theme }) => css`
+    * {
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+    }
+
+    html {
+      font-size: 62.5%;
+    }
+
+    button {
+      cursor: pointer;
+      border: none;
+    }
+
+    html,
+    body {
+      color: ${theme.colors.text};
+      background: ${theme.colors.bg};
+    }
+
+    body,
+    input,
+    button,
+    textarea {
+      font-family: ${theme.font.family};
+      font-weight: ${theme.font.normal};
+      font-size: ${theme.font.sizes.medium};
+    }
+  `}
+`
---
+++ src/styles/theme.ts
@@ -0,0 +1,46 @@
+export const theme = {
+  font: {
+    family:
+      "Nunito, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
+    light: 400,
+    normal: 700,
+    bold: 600,
+    sizes: {
+      xsmall: '1.2rem',
+      small: '1.4rem',
+      medium: '1.6rem',
+      large: '1.8rem',
+      xlarge: '2.0rem',
+      xxlarge: '2.8rem',
+      huge: '5.2rem'
+    }
+  },
+  colors: {
+    white: '#fff',
+    black: '#000',
+    bg: '#EBF2F5',
+    blue: '#15C3D6',
+    yellow: '#FFD666',
+    green: '#3CDC8C',
+    red: '#FF669D',
+    heading: '#4D6F80',
+    text: '#8FA7B2',
+    input: '#F5F8FA',
+    'blue-low': '#D1EDF2',
+    'red-low': '#FFE4EE',
+    'green-low': '#EDFFF6'
+  },
+  layers: {
+    base: 10,
+    menu: 20,
+    overlay: 30,
+    modal: 40,
+    alwaysOnTop: 50
+  },
+  gradient: 'linear-gradient(329.54deg, #29B6D1 0%, #00C7C7 100%)',
+  transition: {
+    default: '0.3s ease-in-out',
+    fast: '0.1s ease-in-out'
+  },
+  shadows: '17.2868px 27.6589px 41.4884px rgba(23, 142, 166, 0.16)'
+}
