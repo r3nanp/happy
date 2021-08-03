7f3328d2c17f5e1be9f87dfeefedf5e732a91a83
Renan Pereira <renanmol87@gmail.com>
2021-08-03 14:00:47 -0300

    feat(app): add styles and head component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,25 @@
 import { AppProps } from 'next/app'
+import { ThemeProvider } from 'styled-components'
+
+import Head from 'next/head'
+import { GlobalStyle } from 'styles/global'
+import { theme } from 'styles/theme'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Head>
+        <title>Happy</title>
+        <link rel="shortcut icon" href="/img/logo.svg" />
+        <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="description" content="Leve felicidade para o mundo!" />
+      </Head>
+
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
