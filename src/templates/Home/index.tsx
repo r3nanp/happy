2077241e46cf2e862a69f4e1ed3da43d1e11e0a7
Renan Pereira <renanmol87@gmail.com>
2021-08-05 12:17:45 -0300

    feat(templates): add content

--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -5,20 +5,29 @@ import * as S from './styles'
 export function HomeTemplate() {
   return (
     <S.Container>
-      <S.ImageWrapper>
-        <Image src="/img/logo-full.svg" alt="Logo" width={500} height={500} />
-      </S.ImageWrapper>
+      <S.Content>
+        <div className="image-wrapper">
+          <Image src="/img/logo-full.svg" alt="Logo" width={500} height={500} />
+        </div>
 
-      <S.Location>
-        <strong>Fortaleza</strong>
-        <span>Ceará</span>
-      </S.Location>
+        <div className="text">
+          <h1>Leve felicidade para o mundo</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+        </div>
+      </S.Content>
 
-      <Link href="/map">
-        <a>
-          <S.ArrowIcon />
-        </a>
-      </Link>
+      <S.Wrapper>
+        <S.Location>
+          <strong>Fortaleza</strong>
+          <span>Ceará</span>
+        </S.Location>
+
+        <Link href="/orphanage">
+          <a className="arrow">
+            <S.ArrowIcon />
+          </a>
+        </Link>
+      </S.Wrapper>
     </S.Container>
   )
 }
--- src/templates/Home/styles.ts
+++ src/templates/Home/styles.ts
@@ -1,18 +1,86 @@
 import styled from 'styled-components'
+import { FaArrowRight } from 'react-icons/fa'
 
 export const Container = styled.main`
   width: 100vw;
   height: 100vh;
   background: ${({ theme }) => theme.gradient};
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding-top: 1rem;
-  padding-bottom: 1rem;
+  display: grid;
+  grid-template-columns: repeat(2, minmax(300px, 1fr));
+
+  .image-wrapper {
+    width: 20rem;
+    height: 10rem;
+  }
 `
 
-export const ImageWrapper = styled.div``
+export const Wrapper = styled.div`
+  position: relative;
 
-export const Location = styled.div``
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: space-between;
+  margin: 1rem;
 
-export const ArrowIcon = styled.div``
+  background: url('/img/children.svg') no-repeat center 80%;
+
+  .arrow {
+    position: absolute;
+    right: 0;
+    bottom: 0;
+    width: 5rem;
+    height: 5rem;
+    background: ${({ theme }) => theme.colors.yellow};
+    border-radius: 25%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    transition: opacity ${({ theme }) => theme.transition.default};
+
+    &:hover {
+      opacity: 0.7;
+    }
+  }
+`
+
+export const Content = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
+
+  h1 {
+    font-size: ${({ theme }) => theme.font.sizes.xxlarge};
+    font-weight: ${({ theme }) => theme.font.bold};
+    line-height: 5rem;
+  }
+
+  p {
+    margin-top: 2.5rem;
+    line-height: 1.5rem;
+  }
+`
+
+export const Location = styled.div`
+  position: absolute;
+  right: 0;
+  top: 0;
+  font-size: ${({ theme }) => theme.font.sizes.medium};
+  line-height: 2rem;
+  display: flex;
+  flex-direction: column;
+  text-align: right;
+
+  > strong {
+    font-weight: ${({ theme }) => theme.font.bold};
+  }
+`
+
+export const ArrowIcon = styled(FaArrowRight)`
+  width: 2rem;
+  height: 2rem;
+
+  color: white;
+`
