e6809edd06304ee00f66b9e8def4e00026493433
Renan Pereira <renanmol87@gmail.com>
2021-08-06 14:39:55 +0000

    styles: create theme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -2,9 +2,9 @@ export const theme = {
   font: {
     family:
       "Nunito, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
-    light: 400,
-    normal: 700,
-    bold: 600,
+    normal: 400,
+    semi: 600,
+    bold: 800,
     sizes: {
       xsmall: '1.2rem',
       small: '1.4rem',
