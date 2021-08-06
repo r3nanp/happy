10c522203231c1d6d551d250a99dbeaab595c213
Renan Pereira <renanmol87@gmail.com>
2021-08-06 22:49:23 +0000

    feat(template): create orphanage template

--- src/templates/Orphanage/index.tsx
+++ src/templates/Orphanage/index.tsx
@@ -1,5 +1,8 @@
 import dynamic from 'next/dynamic'
-import { Sidebar } from 'components/Sidebar'
+import Image from 'next/image'
+import Link from 'next/link'
+
+import * as S from './styles'
 
 export function OrphanageTemplate() {
   const Map = dynamic(() => import('components/Map'), {
@@ -7,9 +10,39 @@ export function OrphanageTemplate() {
   })
 
   return (
-    <>
-      <Sidebar />
-      <Map />
-    </>
+    <S.Container>
+      <S.ContentSidebar>
+        <header>
+          <Image
+            src="/img/marker.svg"
+            alt="Happy Logo"
+            width={400}
+            height={400}
+          />
+
+          <h2 className="title">Escolha um orfanato no mapa</h2>
+          <p>Muitas crianças estão esperando sua visita :)</p>
+        </header>
+
+        <footer>
+          <strong>Fortaleza</strong>
+          <span>Ceará</span>
+
+          <Link href="/">
+            <a className="back-to-menu">
+              <S.ArrowBack />
+            </a>
+          </Link>
+        </footer>
+      </S.ContentSidebar>
+
+      <Map initialLatitude={-3.7305253} initialLongitude={-38.5311193} />
+
+      <Link href="/orphanage/create">
+        <a className="create-orphanage">
+          <S.PlusIcon />
+        </a>
+      </Link>
+    </S.Container>
   )
 }
--- src/templates/Orphanage/styles.ts
+++ src/templates/Orphanage/styles.ts
@@ -1,5 +1,104 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components'
+import { FiPlus, FiArrowLeft } from 'react-icons/fi'
 
 export const Container = styled.div`
+  ${({ theme }) => css`
+    width: 100vw;
+    height: 100vh;
+    position: relative;
+    display: flex;
 
-`;
+    .create-orphanage {
+      z-index: 1000;
+      position: absolute;
+      right: 40px;
+      bottom: 40px;
+
+      width: 5rem;
+      height: 5rem;
+
+      border-radius: 25%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      background: ${theme.colors.blue};
+      transition: filter ${theme.transition.default};
+
+      &:hover {
+        background-color: ${theme.colors['blue-hover']};
+      }
+    }
+  `}
+`
+
+export const ContentSidebar = styled.aside`
+  ${({ theme }) => css`
+    width: 35rem;
+    background: ${theme.colors.blue};
+    padding: 5rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+
+    header {
+      img {
+        width: 7rem;
+      }
+
+      .title {
+        font-size: ${theme.font.sizes.xxlarge};
+        font-weight: ${theme.font.bold};
+        line-height: 2.5rem;
+        margin-top: 4rem;
+      }
+
+      p {
+        line-height: 2.5rem;
+        margin-top: 1.5rem;
+      }
+    }
+
+    footer {
+      display: flex;
+      flex-direction: column;
+      line-height: 1.5rem;
+
+      strong {
+        margin-top: 1rem;
+        font-weight: ${theme.font.bold};
+      }
+
+      span {
+        margin-bottom: 2rem;
+      }
+    }
+
+    .back-to-menu {
+      width: 4rem;
+      height: 4rem;
+
+      background: ${theme.colors.yellow};
+      border-radius: 1.3rem;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      transition: background-color 0.2s;
+
+      &:hover {
+        background-color: ${theme.colors['blue-hover']};
+      }
+    }
+  `}
+`
+
+export const ArrowBack = styled(FiArrowLeft)`
+  width: 2.5rem;
+  height: 2.5rem;
+  color: white;
+`
+
+export const PlusIcon = styled(FiPlus)`
+  width: 2.5rem;
+  height: 2.5rem;
+  color: white;
+`
