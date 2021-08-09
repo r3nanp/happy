4aa9bb03798ae7176e92ed438bb395e80527e57a
Renan Pereira <renanmol87@gmail.com>
2021-08-09 13:32:35 +0000

    chore: change path to the icon

--- src/utils/map-icon/index.ts
+++ src/utils/map-icon/index.ts
@@ -1,7 +1,7 @@
 import * as L from 'leaflet'
 
-export const icon = L.icon({
-  iconUrl: '/icon/marker.svg',
+export const mapIcon = L.icon({
+  iconUrl: '/img/marker.svg',
   iconSize: [58, 68],
   iconAnchor: [29, 68],
   popupAnchor: [0, -60]
