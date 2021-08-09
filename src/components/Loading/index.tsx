6fe29a8bc7c51f6f6a7e30b2835b98aad54d6c29
Renan Pereira <renanmol87@gmail.com>
2021-08-09 18:15:37 +0000

    feat(components): create loading component

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,29 @@
+import { Lottie } from '@crello/react-lottie'
+import styled from 'styled-components'
+import animation from 'animations/loading.json'
+
+export const Wrapper = styled.section`
+  width: 100vw;
+  height: 100vh;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+export function Loading() {
+  return (
+    <Wrapper>
+      <Lottie
+        width="50%"
+        height="50%"
+        className="lottie-container basic"
+        config={{
+          animationData: animation,
+          loop: true,
+          autoplay: true
+        }}
+      />
+    </Wrapper>
+  )
+}
