57bb3839f0fbec84e41391f04aa12def435c5e26
Renan Pereira <renanmol87@gmail.com>
2021-08-09 20:49:31 +0000

    feat: add static data function and types

--- src/pages/orphanages.tsx
+++ src/pages/orphanages.tsx
@@ -1,5 +1,32 @@
+import { GetStaticProps } from 'next'
 import { OrphanagesTemplate } from 'templates/Orphanages'
 
-export default function Orphanages() {
-  return <OrphanagesTemplate />
+type Orphanage = {
+  id: number
+  name: string
+  latitude: number
+  longitude: number
+}
+
+type OrphanagesProps = {
+  orphanages: Orphanage[]
+}
+
+export default function Orphanages({ orphanages }: OrphanagesProps) {
+  return <OrphanagesTemplate orphanages={orphanages} />
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  return {
+    props: {
+      orphanages: [
+        {
+          id: 1,
+          name: 'Reasnkdsand',
+          latitude: -3.7305253,
+          longitude: -38.5311193
+        }
+      ]
+    }
+  }
 }
