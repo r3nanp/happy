be60eb210d4fed85f54d4d5c071db80df44c98fe
Renan Pereira <renanmol87@gmail.com>
2021-08-13 14:32:07 +0000

    feat(next-seo): create seo config

---
+++ next-seo.config.ts
@@ -0,0 +1,23 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+  openGraph: {
+    type: 'website',
+    locale: 'pt_BR',
+    url: 'https://happy.r3nanp.vercel.app',
+    site_name: 'Happy',
+    title: 'Happy',
+    images: [
+      {
+        url: '/img/banner.png',
+        width: 2560,
+        height: 1440,
+        alt: 'Happy'
+      }
+    ]
+  },
+  twitter: {
+    handle: '@r3nanp',
+    site: '@site',
+    cardType: 'summary_large_image'
+  }
+}
