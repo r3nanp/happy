e486bdbe50f67f773dc8c9cb16781154a46e2036
Renan Pereira <renanmol87@gmail.com>
2021-08-15 22:13:27 +0000

    test: add new tests and snapshot

--- src/components/Button/test.spec.tsx
+++ src/components/Button/test.spec.tsx
@@ -3,8 +3,34 @@ import { Button } from '.'
 
 describe('<Button />', () => {
   it('Should be able to render a button', () => {
-    render(<Button>Button</Button>)
+    const { container } = render(<Button>Button</Button>)
 
-    expect(screen.getByText('Button')).toBeInTheDocument()
+    expect(screen.getByRole('button', { name: /button/i })).toHaveStyle({
+      height: '4rem',
+      'border-radius': '1.5rem'
+    })
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
+
+  it('Should render a button in the success page', () => {
+    render(<Button isSuccessPage={true}>Button</Button>)
+
+    expect(screen.getByText('Button')).toHaveStyleRule(
+      'background-color',
+      '#31b272'
+    )
+  })
+
+  it('Should render a disabled Button', () => {
+    render(<Button disabled>Button</Button>)
+
+    expect(screen.getByRole('button', { name: /button/i })).toHaveStyleRule(
+      'cursor',
+      'not-allowed',
+      {
+        modifier: ':disabled'
+      }
+    )
   })
 })
