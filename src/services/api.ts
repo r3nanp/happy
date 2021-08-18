4d26f210b07e93fe226ad7564381ec3de50c0835
Renan Pereira <renanmol87@gmail.com>
2021-08-18 17:30:15 +0000

    feat: add env

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: process.env.APP_URL || 'http://localhost:3000'
+  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000'
 })
