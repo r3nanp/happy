7548972f046cce2e75591acce2f338f2fc85aa59
Renan Pereira <renanmol87@gmail.com>
2021-12-19 15:20:12 -0300

    feat(constants): create GA and mapbox constant files

---
+++ src/constants/ga-tracking.ts
@@ -0,0 +1,1 @@
+export const GA_TRACKING = process.env.NEXT_PUBLIC_GA_TRACKING
---
+++ src/constants/mapbox.ts
@@ -0,0 +1,2 @@
+export const MAPBOX_TOKEN = process.env.NEXT_PUBLIC_MAPBOX_TOKEN
+export const MAP_STYLE_ID = process.env.NEXT_PUBLIC_MAPBOX_STYLE_ID
