9aac29052f1284dc829599e9bee9b279ed6bf2c0
Renan Pereira <renanmol87@gmail.com>
2021-12-29 14:03:03 -0300

    chore(deps): add new deps

--- .storybook/main.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  stories: ['../src/components/**/stories.tsx'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
-  webpackFinal: config => {
-    config.resolve.modules.push(`${process.cwd()}/src`)
-    return config
-  }
-}
