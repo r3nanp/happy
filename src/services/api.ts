5c55066246c7ba19abcb3d113af4b9d92ce5082f
Renan Pereira <renanmol87@gmail.com>
2021-12-29 21:38:34 -0300

    feat(api): add interceptors

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,18 @@
 import axios from 'axios'
+import toast from 'react-hot-toast'
 
 export const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000'
 })
+
+api.interceptors.response.use(
+  response => {
+    return response
+  },
+  error => {
+    const message = error.response?.data?.message || error.message
+
+    toast.error(message)
+    return Promise.reject(message)
+  }
+)
